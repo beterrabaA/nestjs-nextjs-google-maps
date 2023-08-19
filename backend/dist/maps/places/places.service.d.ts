d8ba63ab0729efe9e188163a00126c0e04e87f24
Lucas Vieira <ls6182315@gmail.com>
2023-08-19 00:22:01 -0300

    remove dist folder

--- backend/dist/maps/places/places.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { Client as GoogleMapsClient } from '@googlemaps/google-maps-services-js';
-export declare class PlacesService {
-    private configService;
-    private googleMapsClient;
-    constructor(configService: ConfigService, googleMapsClient: GoogleMapsClient);
-    findPlace(text: string): Promise<import("@googlemaps/google-maps-services-js").FindPlaceFromTextResponseData>;
-}
