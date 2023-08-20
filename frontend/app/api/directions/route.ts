ed01884a380a347298e22d189e656d12cc15d94b
Beterraba <ls6182315@gmail.com>
2023-08-20 00:22:18 -0300

    fix error route

--- frontend/app/api/directions/route.ts
+++ frontend/app/api/directions/route.ts
@@ -1,11 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
+  const apiBaseUrl = process.env.NEXT_PUBLIC_API_ROUTE
   const url = new URL(request.url)
   const originId = url.searchParams.get('originId')
   const destinationId = url.searchParams.get('destinationId')
   const response = await fetch(
-    `http://nestjs:3000/directions?originId=${originId}&destinationId=${destinationId}`,
+    `${apiBaseUrl}/directions?originId=${originId}&destinationId=${destinationId}`,
     {
       next: {
         revalidate: 60,
